714b3af939c01b404f2f5f1bb0d9e954ddbc99aa
Hukeng <hukeng@outlook.de>
2026-03-05 10:54:23 +0100

    implement first cmake and minor adjustments

---
+++ build/CMakeFiles/SDL_Application_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL_Application_test.dir/main.cpp.o"
+  "CMakeFiles/SDL_Application_test.dir/main.cpp.o.d"
+  "SDL_Application_test"
+  "SDL_Application_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDL_Application_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
